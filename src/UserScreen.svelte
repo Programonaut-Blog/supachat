0895d0b10d3576825dd62fb9d27fb38296e946e9
programonaut <dev@maximilian-kuerschner.de>
2022-12-31 19:05:08 +0100

    supabase auth for login screen

---
+++ src/UserScreen.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { currentUser, supabase } from "./lib/supabase";
+
+  async function handleSignOut() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.log("error", error);
+    }
+  }
+</script>
+
+<div class="flex flex-col items-center justify-center w-full py-20">
+  <p>{$currentUser.email}</p>
+  <button
+    class="group relative flex w-1/2 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-2"
+    on:click={handleSignOut}
+  >
+    <span class="absolute inset-y-0 left-0 flex items-center pl-3" />
+    Sign Out
+  </button>
+</div>
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
+import { writable } from 'svelte/store'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export const currentUser = writable((await supabase.auth.getUser()).data.user)
+
+supabase.auth.onAuthStateChange(async () => {
+        currentUser.set((await supabase.auth.getUser()).data.user)
+    }
+)
