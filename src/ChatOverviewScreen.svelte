ce1722ea150007e995e09c263cbcba37367f5eb5
programonaut <dev@maximilian-kuerschner.de>
2023-01-06 18:10:23 +0100

    add on destroy

--- src/ChatOverviewScreen.svelte
+++ src/ChatOverviewScreen.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
     import ChatCard from "./components/ChatCard.svelte";
     import { currentUser, supabase } from "./lib/supabase";
     import type { chat_with_users, chat_with_users_email } from "./types/custom";
@@ -9,66 +9,67 @@
     let allChats: chat_with_users_email[] = [];
     let chatsWatcher;
 
-    async function getAllChats() {
-        // get all chats where the current user is a member
-        const { data: chatIds } = await supabase
+async function getAllChats() {
+    // get all chats where the current user is a member
+    const { data: chatIds } = await supabase
+        .from('chats')
+        .select('id, users:chats_users!inner(user_id)')
+        .eq('users.user_id', $currentUser.id)
+
+    // get all chats with the user profiles
+    return await supabase
+        .from('chats')
+        .select('*, users:chats_users!inner(user:profiles(email))')
+        .in('id', [chatIds.map(chat => chat.id)])
+}
+
+onMount(async () => {
+    ({ data: allChats } = await getAllChats());
+    
+    chatsWatcher = supabase.channel('custom-all-channel')
+        .on(
+            'postgres_changes',
+            { event: '*', schema: 'public', table: 'chats' },
+            async () => {
+                console.log('chats changed');
+                ({ data: allChats } = await getAllChats());
+            }
+        )
+        .subscribe()
+})
+
+async function createChatWithUser() {
+    const {data: otherUser} = await supabase
+        .from('profiles')
+        .select('id')
+        .eq('email', newUserEmail)
+        .single()
+
+    if (otherUser) {
+        const {data: chat} = await supabase
             .from('chats')
-            .select('id, users:chats_users!inner(user_id)')
-            .eq('users.user_id', $currentUser.id)
-
-        // get all chats with the user profiles
-        return await supabase
-            .from('chats')
-            .select('*, users:chats_users!inner(user:profiles(email))')
-            .in('id', [chatIds.map(chat => chat.id)])
-    }
-
-    onMount(async () => {
-        ({ data: allChats } = await getAllChats());
-        
-        chatsWatcher = supabase.channel('custom-all-channel')
-            .on(
-                'postgres_changes',
-                { event: '*', schema: 'public', table: 'chats' },
-                async () => {
-                    console.log('chats changed');
-                    ({ data: allChats } = await getAllChats());
-                }
-            )
-            .subscribe()
-    })
-
-    async function createChatWithUser() {
-        const {data: otherUser} = await supabase
-            .from('profiles')
-            .select('id')
-            .eq('email', newUserEmail)
+            .insert({})
+            .select()
             .single()
 
-        if (otherUser) {
-            const {data: chat} = await supabase
-                .from('chats')
-                .insert({})
-                .select()
-                .single()
-
-            const {data, error} = await supabase
-                .from('chats_users')
-                .insert([
-                    {
-                        chat_id: chat.id,
-                        user_id: $currentUser.id
-                    },
-                    {
-                        chat_id: chat.id,
-                        user_id: otherUser.id
-                    }]
-                )
-
-            console.log(data, error);
-            
-        }
+        const {data, error} = await supabase
+            .from('chats_users')
+            .insert([
+                {
+                    chat_id: chat.id,
+                    user_id: $currentUser.id
+                },
+                {
+                    chat_id: chat.id,
+                    user_id: otherUser.id
+                }]
+            )       
     }
+}
+
+onDestroy(() => {
+    chatsWatcher?.unsubscribe();
+})
 </script>
 
 <div class="p-8 space-y-2">
--- src/ChatScreen.svelte
+++ src/ChatScreen.svelte
@@ -1,61 +1,56 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-    import MessageCard from "./components/MessageCard.svelte";
-    import { currentUser, supabase } from "./lib/supabase";
-    import type { message } from "./types/custom";
+  import { onDestroy, onMount } from "svelte";
+  import MessageCard from "./components/MessageCard.svelte";
+  import { currentUser, supabase } from "./lib/supabase";
+  import type { message } from "./types/custom";
 
-    export let params: { id: string } = {id: ""};
-    let chatId = params.id;
+  export let params: { id: string } = { id: "" };
+  let chatId = params.id;
 
-    let allMessages: message[] = [];
-    let newMessage: string = "";
-    let messagesWatcher;
+  let allMessages: message[] = [];
+  let newMessage: string = "";
+  let messagesWatcher;
 
-    async function getAllMessages() {
-        return await supabase
-            .from('messages')
-            .select('*')
-            .eq('chat_id', chatId)
-    }
+  async function getAllMessages() {
+    return await supabase.from("messages").select("*").eq("chat_id", chatId);
+  }
 
+  onMount(async () => {
+    ({ data: allMessages } = await getAllMessages());
 
-    onMount(async () => {
+    messagesWatcher = supabase
+      .channel("custom-all-channel")
+      .on("postgres_changes", { event: "*", schema: "public", table: "messages" }, async () => {
+        console.log("messages changed");
         ({ data: allMessages } = await getAllMessages());
+      })
+      .subscribe();
+  });
 
-        messagesWatcher = supabase.channel('custom-all-channel')
-            .on(
-                'postgres_changes',
-                { event: '*', schema: 'public', table: 'messages' },
-                async () => {
-                    console.log('messages changed');
-                    ({ data: allMessages } = await getAllMessages());
-                }
-            )
-            .subscribe()
-    })
+  async function sendMessage() {
+    const { data, error } = await supabase.from("messages").insert([
+      {
+        chat_id: parseInt(chatId),
+        author_id: $currentUser.id,
+        content: newMessage,
+      },
+    ]);
+  }
 
-    async function sendMessage() {
-        const {data, error} = await supabase
-            .from('messages')
-            .insert([
-                {
-                    chat_id: parseInt(chatId),
-                    author_id: $currentUser.id,
-                    content: newMessage
-                }
-            ])
-    }
+  onDestroy(() => {
+    messagesWatcher?.unsubscribe();
+  });
 </script>
 
 <div class="flex flex-col space-y-1 p-2">
-    {#each allMessages as message}
-        <MessageCard own={message.author_id === $currentUser.id}>
-            {message.content}
-        </MessageCard>
-    {/each}
+  {#each allMessages as message}
+    <MessageCard own={message.author_id === $currentUser.id}>
+      {message.content}
+    </MessageCard>
+  {/each}
 </div>
 
 <div class="absolute bottom-0 flex w-full space-x-4">
-    <input bind:value={newMessage} type="text" placeholder="Message" class="flex-1" />
-    <button on:click={sendMessage} class="bg-gray-200 rounded p-2 hover:bg-gray-300">Send</button>
+  <input bind:value={newMessage} type="text" placeholder="Message" class="flex-1" />
+  <button on:click={sendMessage} class="bg-gray-200 rounded p-2 hover:bg-gray-300">Send</button>
 </div>
